f06071eaeb5b5c5d501a18b3748e930a12697742
Ben Schwartz <benschw@gmail.com>
2016-10-21 16:04:01 -0500

    adding tests, problems with autoloading

--- test/FLogTest.php
+++ test/FLogTest.php
@@ -3,19 +3,27 @@ namespace Fliglio\Logging;
 
 
 class FlogTest extends \PHPUnit_Framework_TestCase {
+	use Flog;
+
+	private $l;
 
 	public function setUp() {
+		$l = new StubLogger();
+		FlogRegistry::set(new Flogger($this->l));
+
 	}
 
-	/**
-	 * Test that a simple model can be validated
-	 */
-	public function testValidation() {
-
-		// given
-		$foo = new Foo();
+	public function testFlog() {
 
 		// when
-		$foo->doIt();
+		$this->log()->info("foo");                     //foo
+		$this->log()->context()->add("bar", "baz");
+		$this->log()->info("foo");                     //bar=baz, foo
+		$this->log()->info("foo", ["bar" => "boo"]);   //bar=boo, foo
+		$this->log()->info("foo");                     //bar=baz, foo
+
+
+		// then
+		$this->assertEquals($this->l->store, [], "these should match");
 	}
 }
